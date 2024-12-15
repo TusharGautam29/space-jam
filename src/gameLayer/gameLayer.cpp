56a5f81cc1b06738b90731d400d52d1e36c0edc9
TusharGautam29 <tushar.gautam.23033@iitgoa.ac.in>
2024-12-15 23:15:55 +0530

    ship rotation

--- src/gameLayer/gameLayer.cpp
+++ src/gameLayer/gameLayer.cpp
@@ -17,12 +17,18 @@ struct GameplayData
 	glm::vec2 playerPos = { 100,100 };
 };
 
+
 GameplayData data;
 
 gl2d::Renderer2D renderer;
-gl2d::Texture spaceship_texture;
-gl2d::Texture background_texture[4];
-TiledRenderer tiledRenderer[4];
+
+constexpr int BACKGROUNDS = 4;
+
+gl2d::Texture spaceShipsTexture;
+gl2d::TextureAtlasPadding spaceShipsAtlas;
+
+gl2d::Texture backgroundTexture[BACKGROUNDS];
+TiledRenderer tiledRenderer[BACKGROUNDS];
 
 bool initGame()
 {
@@ -30,70 +36,135 @@ bool initGame()
 	gl2d::init();
 	renderer.create();
 
-	spaceship_texture.loadFromFile(RESOURCES_PATH "spaceShip/ships/purple.png", true);
-	background_texture[0].loadFromFile(RESOURCES_PATH "background1.png", true);
-	background_texture[1].loadFromFile(RESOURCES_PATH "background2.png", true);
-	background_texture[2].loadFromFile(RESOURCES_PATH "background3.png", true);
-	background_texture[3].loadFromFile(RESOURCES_PATH "background4.png", true);
-	
-	tiledRenderer[0].texture = background_texture[0];
-	tiledRenderer[1].texture = background_texture[1];
-	tiledRenderer[2].texture = background_texture[2];
-	tiledRenderer[3].texture = background_texture[3];
-	
+	spaceShipsTexture.loadFromFileWithPixelPadding
+	(RESOURCES_PATH "spaceShip/stitchedFiles/spaceships.png", 128, true);
+	spaceShipsAtlas = gl2d::TextureAtlasPadding(5, 2, spaceShipsTexture.GetSize().x, spaceShipsTexture.GetSize().y);
+
+	backgroundTexture[0].loadFromFile(RESOURCES_PATH "background1.png", true);
+	backgroundTexture[1].loadFromFile(RESOURCES_PATH "background2.png", true);
+	backgroundTexture[2].loadFromFile(RESOURCES_PATH "background3.png", true);
+	backgroundTexture[3].loadFromFile(RESOURCES_PATH "background4.png", true);
+
+	tiledRenderer[0].texture = backgroundTexture[0];
+	tiledRenderer[1].texture = backgroundTexture[1];
+	tiledRenderer[2].texture = backgroundTexture[2];
+	tiledRenderer[3].texture = backgroundTexture[3];
+
 	tiledRenderer[0].paralaxStrength = 0;
 	tiledRenderer[1].paralaxStrength = 0.2;
 	tiledRenderer[2].paralaxStrength = 0.4;
 	tiledRenderer[3].paralaxStrength = 0.7;
+
 	return true;
 }
 
 
+constexpr float shipSize = 250.f;
 
 bool gameLogic(float deltaTime)
 {
+
 #pragma region init stuff
 	int w = 0; int h = 0;
 	w = platform::getFrameBufferSizeX(); //window w
 	h = platform::getFrameBufferSizeY(); //window h
-	
+
 	glViewport(0, 0, w, h);
 	glClear(GL_COLOR_BUFFER_BIT); //clear screen
 
 	renderer.updateWindowMetrics(w, h);
 #pragma endregion
-#pragma region background
-	renderer.currentCamera.zoom = 0.5;
-	for(int i=0;i<=3;i++)tiledRenderer[i].render(renderer);
-#pragma endregion
 
 
 
 #pragma region movement
+
 	glm::vec2 move = {};
-	if (platform::isButtonHeld(platform::Button::W) || platform::isButtonHeld(platform::Button::Up)) {
-		move.y=-1;
+
+	if (
+		platform::isButtonHeld(platform::Button::W) ||
+		platform::isButtonHeld(platform::Button::Up)
+		)
+	{
+		move.y = -1;
 	}
-	if (platform::isButtonHeld(platform::Button::A) || platform::isButtonHeld(platform::Button::Left)) {
-		move.x = -1;
-	}
-	if (platform::isButtonHeld(platform::Button::S) || platform::isButtonHeld(platform::Button::Down)) {
+	if (
+		platform::isButtonHeld(platform::Button::S) ||
+		platform::isButtonHeld(platform::Button::Down)
+		)
+	{
 		move.y = 1;
 	}
-	if (platform::isButtonHeld(platform::Button::D) || platform::isButtonHeld(platform::Button::Right)) {
+	if (
+		platform::isButtonHeld(platform::Button::A) ||
+		platform::isButtonHeld(platform::Button::Left)
+		)
+	{
+		move.x = -1;
+	}
+	if (
+		platform::isButtonHeld(platform::Button::D) ||
+		platform::isButtonHeld(platform::Button::Right)
+		)
+	{
 		move.x = 1;
 	}
-	if (move.x != 0 || move.y != 0) {
-		move=glm::normalize(move);
-		move *=deltaTime*1000; //1000 pixels per second;
-		data.playerPos +=move;
+
+	if (move.x != 0 || move.y != 0)
+	{
+		move = glm::normalize(move);
+		move *= deltaTime * 2000; //500 pixels per seccond
+		data.playerPos += move;
 	}
 
 #pragma endregion
 
-	renderer.currentCamera.follow(data.playerPos, deltaTime * 200, 10, 200, w, h);
-	renderer.renderRectangle({data.playerPos, 150, 150}, spaceship_texture); //{Coordinates,size of rectagnle},texture
-	
+#pragma region follow
+
+	renderer.currentCamera.follow(data.playerPos
+		, deltaTime * 550, 1, 150, w, h);
+
+#pragma endregion
+
+#pragma region render background
+
+	renderer.currentCamera.zoom = 0.5;
+
+	for (int i = 0; i < BACKGROUNDS; i++)
+	{
+		tiledRenderer[i].render(renderer);
+	}
+#pragma endregion
+
+
+#pragma region mouse pos
+
+	glm::vec2 mousePos = platform::getRelMousePosition();
+	glm::vec2 screenCenter(w / 2.f, h / 2.f);
+
+	glm::vec2 mouseDirection = mousePos - screenCenter;
+
+	if (glm::length(mouseDirection) == 0.f)
+	{
+		mouseDirection = { 1,0 };
+	}
+	else
+	{
+		mouseDirection = normalize(mouseDirection);
+	}
+
+	float spaceShipAngle = atan2(mouseDirection.y, -mouseDirection.x);
+
+#pragma endregion
+
+
+#pragma region render ship
+
+	renderer.renderRectangle({ data.playerPos - glm::vec2(shipSize / 2,shipSize / 2), shipSize,shipSize }, spaceShipsTexture,Colors_White, {}, glm::degrees(spaceShipAngle) + 90.f,spaceShipsAtlas.get(1, 0));
+
+#pragma endregion
+
+
 
 	renderer.flush();
 
